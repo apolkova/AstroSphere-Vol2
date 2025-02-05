64ab648cb361e9de2f9226f19a5e3e7d97837e2c
apolkova <a_polkova@utb.cz>
2025-02-06 00:08:38 +0100

    icon and splashscreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'AstroSphere',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, 
+      backgroundColor: "#000000", 
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
