0b616a3783a87079dda3434e7d3fe3b33b35ace6
apolkova <a_polkova@utb.cz>
2025-02-05 23:01:12 +0100

    tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../services/news.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  newsArticles: any[] = [];
+  isLoading: boolean = false;
 
-  constructor() {}
+  constructor(private spaceNewsService: NewsService) {}
+
+  ngOnInit() {
+    this.loadSpaceNews();
+  }
+
+  loadSpaceNews() {
+    this.isLoading = true;
+    this.spaceNewsService.fetchNews().subscribe({
+      next: (data: any) => {
+        console.log('News Articles:', data);
+        this.newsArticles = data.results || [];
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching space news:', error);
+        this.isLoading = false;
+      }
+    });    
+  }
+
+  openArticle(url: string) {
+    window.open(url, '_blank');
+  }
 
 }
